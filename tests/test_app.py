f4497f55a31ed9aad79a2399befac136e1589423
rodrigocam <rodrigo.redcode@gmail.com>
2018-10-31 14:44:30 -0300

    Add test for get pingout with a valid and invalid uuid

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,8 +8,31 @@ def test_return_200_on_root(client):
 def test_unique_generated_uuid(client):
     response = client.post('/create-pingout')
     uuid_1 = response.json['uuid']  
+    assert response.status_code == 201
 
     response = client.post('/create-pingout')  
     uuid_2 = response.json['uuid']
-    
+    assert response.status_code == 201
+
     assert uuid_1 != uuid_2
+
+
+def test_ping_to_valid_pingout_uuid(client):
+    response = client.post('/create-pingout')
+    uuid = response.json['uuid']
+    client.post(f'/{uuid}/ping')
+    response = client.get(f'/{uuid}')
+    assert response.status_code == 200
+
+
+def test_ping_to_invalid_pingout_uuid(client):
+    invalid_uuid = '84b9cfd3b8124023b8be4d43720d179a'
+    response = client.get(f'/{invalid_uuid}')
+    assert response.status_code == 404
+
+
+def test_filtered_pings(client):
+    response = client.post('/create-pingout')
+    uuid = response.json['uuid']
+
+    response = client.get(f'/{uuid}/filter/?initial_date=&final')
