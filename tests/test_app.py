ae2a8559668cc78e512c879ee04c80f0881d3a1a
rodrigocam <rodrigo.redcode@gmail.com>
2018-10-31 14:30:39 -0300

    Add test for unique uuid generated by /create-pingout route

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,3 +3,13 @@ def test_return_200_on_root(client):
     """ Return status code 200 on root url"""
     response = client.get('/')
     assert response.status_code == 200
+
+
+def test_unique_generated_uuid(client):
+    response = client.post('/create-pingout')
+    uuid_1 = response.json['uuid']  
+
+    response = client.post('/create-pingout')  
+    uuid_2 = response.json['uuid']
+    
+    assert uuid_1 != uuid_2
