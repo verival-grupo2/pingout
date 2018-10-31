a6f1e286a64f20a6cb98615844cedb7ae661ccfe
pablodiegoss <pablodiegoss@hotmail.com>
2018-10-31 15:30:13 -0300

    Create Test for json to csv

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import pytest
-from pingout.utils import validate_uuid
+from pingout.utils import validate_uuid, from_json_to_csv
 from uuid import uuid4
 
 
@@ -10,4 +10,11 @@ def test_validate_uuid():
 
     valid_uuid = "84b9cfd3b8124023b8be4d43720d179a"
     res = validate_uuid(valid_uuid)
-    assert res == True    
+    assert res == True
+
+def test_convert_json_to_csv():
+    json = {"CleberLixo": {"nome": "ASDADSADS", "id": 12123, "telefone":"997446446", "endereco":"iasjddjsaisd"},
+            "IgorLixo": {"nome": "ASDADSADS", "id": 12123, "telefone":"997446446", "endereco":"iasjddjsaisd"},
+            "RodrigoLixo": {"nome": "ASDADSADS", "id": 12123, "telefone":"997446446", "endereco":"iasjddjsaisd"}}
+    from_json_to_csv(json,"testeConvert")
+    assert True
