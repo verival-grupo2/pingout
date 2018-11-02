dc693c4e22fad956adb3d366fdd3b27d75b695c8
Vinicius Ferreira <vfbdelima@gmail.com>
2018-11-02 16:42:58 -0300

    Add tests for function get_pingouts_occur_range_date
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>
    Co-authored-by: Lucas Hiroshi <lucas.hh@hotmail.com>

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,7 +1,29 @@
 import pytest
 import requests
+import json
+from uuid import uuid4
 
 def test_create_pingout():
     response = requests.get('http://0.0.0.0:5000/create-pingout')
 
     assert response.status_code == 405
+
+def test_get_pingouts_occur_range_date_error_404():
+    uuid = uuid4()
+
+    response = requests.get('http://0.0.0.0:5000/{}/filter'.format(uuid.hex))
+
+    assert response.status_code == 404
+
+"""
+def test_get_pingouts_occur_range_date_error_type():
+    response = requests.post('http://0.0.0.0:5000/create-pingout')
+
+    data = json.loads(response.text)
+
+    payload = {'initial_date': '2018-11-01', 'final_date': '2018-11-02'}
+
+    response = requests.get('http://0.0.0.0:5000/{}/filter'.format(data['uuid']), params=payload)
+
+    assert response.status_code == 400
+"""
